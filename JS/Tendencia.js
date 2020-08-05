0c3c6e1a35d10f722950616440e59709741ebf6f
juandafonseca <danifonsedela@gmail.com>
2020-08-05 12:56:37 -0500

    Se agrega la seccion se resultados de busqueda y se crea el javascript de la sección de tendencias

---
+++ JS/Tendencia.js
@@ -0,0 +1,83 @@
+function tendencias() {
+  let apikey = "8Oc7NtbE0yskmJrjyzaHNBxjHycKZ3pp"
+  let urltendencia = `https://api.giphy.com/v1/gifs/trending?api_key=${apikey}&limit=16&offset=0&rating=G&lang=e`;
+  let Mostrar_resultado = document.getElementById('gridtendencias');
+  
+
+  fetch(urltendencia)
+    .then(response => {
+      return response.json();
+
+    })
+
+    .then(json => {
+      console.log(json)
+
+      json.data.forEach(item => {
+
+        const contenedor_imagen = document.createElement('div')
+        contenedor_imagen.classList.add('gridcontenedorimagen');
+        Mostrar_resultado.appendChild(contenedor_imagen);
+
+        const nuevoGif = document.createElement('img')
+        nuevoGif.src = item.images.original.url;
+        contenedor_imagen.appendChild(nuevoGif);
+
+        const divtitle = document.createElement('div')
+        divtitle.classList.add('footer');
+        contenedor_imagen.appendChild(divtitle);
+
+        const gtitle = document.createElement('p')
+        divtitle.appendChild(gtitle);
+        gtitle.innerHTML = item.title;
+
+      })
+    })
+
+    .catch(err => console.log(err));
+}
+tendencias();
+
+
+
+
+
+
+
+function resultados_de_busquedas() {
+  document.getElementById("tendenciatitulo").innerHTML = 'Resultados de busqueda' // Cambio el título  
+  document.getElementById("gridtendencias").innerHTML = '' //dejamos el contenido del div gidtrendin vacio
+  let Mostrar_resultado = document.getElementById('gridtendencias')
+  let apikey = "8Oc7NtbE0yskmJrjyzaHNBxjHycKZ3pp"
+  let x = document.getElementById("inputbusqueda").value
+  let url = `https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${x}&limit=16&offset=0&rating=G&lang=e`;
+
+
+  fetch(url)
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+      json.data.forEach(item => {
+        var gifurl = item.images.original.url;
+
+        const contenedor_imagen = document.createElement('div');
+        Mostrar_resultado.appendChild(contenedor_imagen);
+        contenedor_imagen.classList.add('gridcontenedorimagen');
+
+        const nuevo_Gif = document.createElement('img');
+        contenedor_imagen.appendChild(nuevo_Gif);
+        nuevo_Gif.src = gifurl;
+        nuevo_Gif.classList.add('gifBuscado');
+
+        const divtitle = document.createElement('div')
+        divtitle.classList.add('footer');
+        contenedor_imagen.appendChild(divtitle);
+
+        const gtitle = document.createElement('p')
+        divtitle.appendChild(gtitle);
+        gtitle.innerHTML = item.title;
+      })
+    })
+    .catch(err => console.log(err));
+}
--- JS/menubuscar.js
+++ JS/menubuscar.js
@@ -1,5 +1,6 @@
 function mostrarmenu(){
 
+   document.getElementById("tipobusquedas").style.height="160px"
     document.getElementById("tipobusquedas").style.visibility='visible'
 
 }
@@ -7,3 +8,5 @@ function mostrarmenu(){
 function ocultarmenu(){
     document.getElementById("tipobusquedas").style.visibility='hidden'
 }
+
+window.onload =  ocultarmenu()
